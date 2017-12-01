a345d8df1f466dbf402bc1bdd8de208ce88e3615
Ryon <league@Mac-mini-4.local>
2017-11-30 16:57:19 -0800

    Baby steps
    y0U bEttER HurRy uP bOIIII

---
+++ GNGamePanel.java
@@ -0,0 +1,4 @@
+
+public class GNGamePanel extends JPanel{  } {
+
+}
---
+++ GameName.java
@@ -0,0 +1,32 @@
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame; 
+public class GameName {
+	  int width = 800;
+		int height= 380;
+		JFrame frame =new JFrame();
+	public static void main(String[] args) {
+	GameName window = new GameName();
+	
+	
+	
+  
+	
+}
+GameName(){
+	setup();
+	
+}
+
+void setup(){
+	frame.setVisible(true);
+	 frame.setSize(width, height);
+	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+
+	
+	
+}
+}
+
+
+
