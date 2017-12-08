9c569f454ccbfdfd9b56b0ea2f22f4b00c6d9f69
Ryon <ryonmkkan@outlook.com>
2017-12-07 16:58:37 -0800

    Add files via upload

--- GNGamePanel.java
+++ GNGamePanel.java
@@ -1,4 +1,33 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GNGamePanel extends JPanel{  } {
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GNGamePanel extends JPanel  implements ActionListener{   
+Timer time;
+GNGamePanel(){
+	time = new Timer(1000 / 60,this);
+	
+	
+}
+void StartGame(){
+	time.start();	
+	
+}
+
+
+
+
+
+
+
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	System.out.println("I");
+}
 
 }
--- GameName.java
+++ GameName.java
@@ -3,8 +3,10 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame; 
 public class GameName {
 	  int width = 800;
-		int height= 380;
+		int height= 38;
 		JFrame frame =new JFrame();
+		//GNGamePanel = GNGamePPanel;
+		
 	public static void main(String[] args) {
 	GameName window = new GameName();
 	
@@ -14,11 +16,14 @@ public class GameName {
 	
 }
 GameName(){
+ GNGamePanel p = new GNGamePanel();
 	setup();
 	
 }
 
 void setup(){
+	
+	
 	frame.setVisible(true);
 	 frame.setSize(width, height);
 	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
