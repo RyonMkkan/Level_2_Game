0a6913b2fef7d33b9e85937dc1d9df75592dd557
Ryon <league@Mac-mini-3.local>
2018-01-04 16:54:45 -0800

    fixed some errors
    lool

--- game/GameName.java
+++ game/GameName.java
@@ -1,14 +1,14 @@
-import java.awt.event.ActionListener;
+import javax.swing.JFrame;
 
-import javax.swing.JFrame; 
 public class GameName {
 	  int width = 800;
 		int height= 38;
 		JFrame frame =new JFrame();
-		//GNGamePanel = GNGamePPanel;
+		GNGamePanel panel = new GNGamePanel();
 		
 	public static void main(String[] args) {
 	GameName window = new GameName();
+	
 	
 	
 	
@@ -27,6 +27,8 @@ void setup(){
 	frame.setVisible(true);
 	 frame.setSize(width, height);
 	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	 
+	 
 
 	
 	
