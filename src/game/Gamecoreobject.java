32b65d0e22948037e77f4519e57889654413a54c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-15 18:02:28 -0800

    Made another class

---
+++ src/game/Gamecoreobject.java
@@ -0,0 +1,28 @@
+package game;
+
+public class Gamecoreobject {
+	int x;
+
+    int y;
+
+    int width;
+
+    int height;
+	
+	
+	
+	Gamecoreobject(){
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
